dbeed621c914a0fddd9df678439f29bcfbd496fb
khashei <khashei@gmail.com>
2020-04-20 08:01:58 -0700

    changed inline-help to functional

--- src/pages/TakBeyt/input-form/inline-help.jsx
+++ src/pages/TakBeyt/input-form/inline-help.jsx
@@ -1,64 +1,90 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Typography, Card, CardContent, CardActions, Button, Popper } from '@material-ui/core/';
-import withStyles from '@material-ui/core/styles/withStyles';
+import { makeStyles } from '@material-ui/core/styles';
 
-class InlineHelp extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      arrowRef: null,
-    };
-  }
+const useStyles = makeStyles(
+  (theme) => ({
+    card: {
+      minWidth: 200,
+    },
+    button: {
+      margin: theme.spacing.unit,
+    },
+    popper: {
+      zIndex: 1,
+      '& $arrow': {
+        top: 0,
+        left: 0,
+        marginTop: '-0.9em',
+        width: '3em',
+        height: '1em',
+        '&::before': {
+          borderWidth: '0 1em 1em 1em',
+          borderColor: `transparent transparent ${theme.palette.common.white} transparent`,
+        },
+      },
+    },
+    arrow: {
+      fontSize: 7,
+      width: '3em',
+      height: '3em',
+      '&::before': {
+        content: '""',
+        margin: 'auto',
+        display: 'block',
+        width: 0,
+        height: 0,
+        borderStyle: 'solid',
+      },
+    },
+  })
+);
 
-  setArrowRef = (node) => {
-    this.setState({
-      arrowRef: node,
-    });
-  };
-
-  render() {
-    const { classes, anchor } = this.props;
-    return (
-      <Popper
-        placement="bottom"
-        disablePortal
-        open
-        anchorEl={anchor}
-        className={classes.popper}
-        modifiers={{
-          flip: {
-            enabled: true,
-          },
-          preventOverflow: {
-            enabled: true,
-            boundariesElement: 'window',
-          },
-          arrow: {
-            enabled: true,
-            element: this.state.arrowRef,
-          },
-        }}
-      >
-        <span className={classes.arrow} ref={this.setArrowRef} />
-        <Card className={classes.card}>
-          <CardContent>
-            <Typography>اگر خواهی بگویی شعر نابی</Typography>
-            <Typography>بپرس از من بگویم تا جوابی</Typography>
-            <Typography>بده الگـوی بیـتی را به بلبـل</Typography>
-            <Typography>همانند مثال ای دسته‌ی گل</Typography>
-          </CardContent>
-          <CardActions>
-            <Button size="small" className={classes.button} onClick={this.props.onRandomSampleClick} >تولید الگوی نمونه</Button>
-          </CardActions>
-        </Card>
-      </Popper>
-    );
-  }
+const InlineHelp = props => {
+  const [arrowRef, setArrowRef] = useState();
+  const classes = useStyles();
+  return (
+    <Popper
+      placement="bottom"
+      disablePortal
+      open
+      anchorEl={props.anchor}
+      className={classes.popper}
+      modifiers={{
+        flip: {
+          enabled: true,
+        },
+        preventOverflow: {
+          enabled: true,
+          boundariesElement: 'window',
+        },
+        arrow: {
+          enabled: true,
+          element: arrowRef,
+        },
+      }}
+    >
+      <span className={classes.arrow} ref={setArrowRef.bind(this)} />
+      <Card className={classes.card}>
+        <CardContent>
+          <Typography>اگر خواهی بگویی شعر نابی</Typography>
+          <Typography>بپرس از من بگویم تا جوابی</Typography>
+          <Typography>بده الگـوی بیـتی را به بلبـل</Typography>
+          <Typography>همانند مثال ای دسته‌ی گل</Typography>
+        </CardContent>
+        <CardActions>
+          <Button
+            size="small"
+            className={classes.button}
+            onClick={props.onRandomSampleClick}>تولید الگوی نمونه</Button>
+        </CardActions>
+      </Card>
+    </Popper>
+  );
 }
 
 InlineHelp.propTypes = {
-  classes: PropTypes.object.isRequired,
   onRandomSampleClick: PropTypes.func.isRequired,
   anchor: PropTypes.object,
 };
@@ -67,40 +93,4 @@ InlineHelp.defaultProps = {
   anchor: null,
 };
 
-const styles = theme => ({
-  card: {
-    minWidth: 200,
-  },
-  button: {
-    margin: theme.spacing.unit,
-  },
-  popper: {
-    zIndex: 1,
-    '& $arrow': {
-      top: 0,
-      left: 0,
-      marginTop: '-0.9em',
-      width: '3em',
-      height: '1em',
-      '&::before': {
-        borderWidth: '0 1em 1em 1em',
-        borderColor: `transparent transparent ${theme.palette.common.white} transparent`,
-      },
-    },
-  },
-  arrow: {
-    fontSize: 7,
-    width: '3em',
-    height: '3em',
-    '&::before': {
-      content: '""',
-      margin: 'auto',
-      display: 'block',
-      width: 0,
-      height: 0,
-      borderStyle: 'solid',
-    },
-  },
-});
-
-export default withStyles(styles, { withTheme: true })(InlineHelp);
+export default InlineHelp;
