e8bf1a3c5c56155c57194227c2698d321a8d306a
khashei <khashei@gmail.com>
2020-04-30 07:21:19 -0700

    Added Samim Font to the theme and webpack

--- src/pages/App/theme.js
+++ src/pages/App/theme.js
@@ -1,8 +1,15 @@
 import { red } from '@material-ui/core/colors';
 import { createMuiTheme } from '@material-ui/core/styles';
+import '../../resources/fonts.css';
+
+const primaryFontFamily = '"Samim", "Tahoma", "Helvetica", "Arial", sans-serif';
 
 // A custom theme for this app
 const theme = createMuiTheme({
+  typography: {
+    useNextVariants: true,
+    fontFamily: primaryFontFamily,
+  },
   palette: {
     primary: {
       main: '#615136',
