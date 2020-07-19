72eb37ff3f18c39c8db60b08131516935dd269e1
khashei <khashei@gmail.com>
2020-07-19 11:39:36 -0700

    updated 404 page layout

--- src/pages/Errors/NotFound.jsx
+++ src/pages/Errors/NotFound.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
+import { Container, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 import { Link } from 'react-router-dom';
 
+const useStyles = makeStyles(() => ({
+  root: {
+    paddingRight: 15,
+    paddingLeft: 15,
+    marginTop: 30,
+    marginRight: 'auto',
+    marginLeft: 'auto',
+  },
+}));
+
 function NotFound() {
+  const classes = useStyles();
   return (
-    <div>
-      <h1>Page not found</h1>
-      <Link to="/">You can go home, maybe?</Link>
-    </div>
+    <Container className={classes.root}>
+      <Typography variant="h5">یافت می‌نشود گشته‌ایم ما</Typography>
+      <Link to="/about">درباره بلبل زبان</Link>
+    </Container>
   );
 }
 
