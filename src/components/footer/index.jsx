18f5ba712711f6ef6c3c894987e7a0d6ff42bf1e
khashei <khashei@gmail.com>
2020-08-15 09:30:07 -0700

    added GA to sharing

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,7 +1,16 @@
+/* eslint-disable import/no-unresolved */
 import React from 'react';
 import { Grid, Button, Typography, ButtonGroup } from '@material-ui/core';
+import * as gtag from '@utils/gtag';
 
 const Footer = () => {
+  const postAnalyticEvent = (action) => () => {
+    gtag.event({
+      action: `visit_${action}`,
+      category: 'link',
+    });
+  };
+
   return (
     <Grid container alignItems="center" direction="column">
       <Typography variant="body1" style={{ marginTop: 20 }}>
@@ -13,16 +22,32 @@ const Footer = () => {
           variant="text"
           color="primary"
         >
-          <Button target="_blank" href="https://www.instagram.com/bolbolzaban">
+          <Button
+            target="_blank"
+            href="https://www.instagram.com/bolbolzaban"
+            onClick={postAnalyticEvent('Instagram')}
+          >
             اینستاگرام
           </Button>
-          <Button target="_blank" href="https://www.facebook.com/bolbolzaban">
+          <Button
+            target="_blank"
+            href="https://www.facebook.com/bolbolzaban"
+            onClick={postAnalyticEvent('Facebook')}
+          >
             فیس‌بوک
           </Button>
-          <Button target="_blank" href="https://twitter.com/bolbol_zaban">
+          <Button
+            target="_blank"
+            href="https://twitter.com/bolbol_zaban"
+            onClick={postAnalyticEvent('Twitter')}
+          >
             توییتر
           </Button>
-          <Button target="_blank" href="https://t.me/bolbol_zaban">
+          <Button
+            target="_blank"
+            href="https://t.me/bolbol_zaban"
+            onClick={postAnalyticEvent('Telegram')}
+          >
             تلگرام
           </Button>
         </ButtonGroup>
