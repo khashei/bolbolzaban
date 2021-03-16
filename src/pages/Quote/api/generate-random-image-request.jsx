1d3bb655fd2b8addfa130067f49f09c912e47dfa
khashei <khashei@gmail.com>
2021-03-15 19:47:10 -0700

    Generate noroozy quotes

--- src/pages/Quote/api/generate-random-image-request.jsx
+++ src/pages/Quote/api/generate-random-image-request.jsx
@@ -3,8 +3,12 @@ import { getRequest } from '@utils/request';
 import API_BASE_PATH from '@app-settings';
 
 const generateRandomImageRequest = async () => {
+  const inputs = ['(مصرع) نوروز', '(مصرع) عید', '(مصرع) بهار', '(مصرع) مبارک', ''];
+
   return getRequest({
-    url: `${API_BASE_PATH}/deeptext/randomimage`,
+    url: `${API_BASE_PATH}/deeptext/randomimage?context=${
+      inputs[Math.floor(Math.random() * inputs.length)]
+    }`,
   });
 };
 
